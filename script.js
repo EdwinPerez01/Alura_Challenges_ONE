caae027d7f27129a72533cad0208de5c0be29f2b
EdwinPerez01 <perez01.edwin01@gmail.com>
2024-08-16 16:27:34 -0600

    agregando el challenge v1.0

---
+++ script.js
@@ -0,0 +1,55 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+
+/*Las "llaves" de encriptación que utilizaremos son las siguientes:
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"*/
+
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+}
+
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+    stringEncriptada = stringEncriptada.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+
+    }
+    return stringEncriptada
+}
+
+
+function btnDesncriptar(){
+    const textoDesencriptado = desencriptar(textArea.value)
+    mensaje.value = textoDesencriptado
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+}
+
+
+function desencriptar(stringDesncriptado){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+    stringDesncriptado = stringDesncriptado.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesncriptado.includes(matrizCodigo[i][1])){
+            stringDesncriptado = stringDesncriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+
+    }
+    return stringDesncriptado
+}
+
